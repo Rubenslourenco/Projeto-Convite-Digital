7e4ec9c2b8499fd39abf68739b706131c1ea3cfb
Rubenslourenco <alvesrubens926@gmail.com>
2024-11-21 08:42:40 -0300

    Teste

---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Pagina from "@/components/template/Pagina";
+export default function Home() {
+  return (
+    <div className="w-full">
+      <Pagina>
+        <div>Inicio</div>
+      </Pagina>
+    </div>
+  );
+}
