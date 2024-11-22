2f711cdc65ecc88a4c2c2b2a7e0a1c6ffc146473
Rubens Alves <alvesrubens926@gmail.com>
2024-11-21 23:38:14 -0300

    Inicio das rotas

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,12 +1,24 @@
-import Pagina from "@/components/template/pagina";
-import { Evento } from "@/core";
+import LogoGrande from "@/components/template/LogoGrande";
+import Link from "next/link"; // Update the path to the correct module
+
 export default function Home() {
-  const e: Partial<Evento> = {};
   return (
-    <div className="w-full">
-      <Pagina>
-        <div>Inicio</div>
-      </Pagina>
+    <div
+      className="
+        h-screen flex flex-col items-center justify-center gap-10
+        bg-[url('/background-elementos.svg')] bg-cover
+        "
+    >
+      <div className=" flex flex-col items-center gap-4 ">
+        <LogoGrande />
+        <p className=" text-zinc-500 font-light w-96 leading-6 text-center select-none">
+          Crie gerencie o convite do seu evento de forma rapida e facil. sem
+          complicação
+        </p>
+      </div>
+      <Link href="/evento" className=" botao azul text-lg uppercase">
+        Crie o seu evento
+      </Link>
     </div>
   );
 }
